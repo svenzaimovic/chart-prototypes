9fb03eb818cf1fe671d9d21df6046bbb6f7e0f0a
Sven Zaimovic <svenzaimovic@gmail.com>
2020-05-18 15:48:15 +0200

    line zoom chart

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import StackedBarChart from '../views/StackedBarChart.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/stacked-bar-chart',
+    name: 'Stacked Bar Chart',
+    component: StackedBarChart
   },
   {
     path: '/about',
